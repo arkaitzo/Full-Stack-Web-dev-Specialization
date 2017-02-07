4ea0fafcf57c78b9e52f8509d81243da9dc5c89c
Arkaitz Etxezarreta <arkaitzo@gmail.com>
2017-02-07 16:53:47 +0100

    Copying the files and cleaning up the dist folder

--- 03-Front-End JavaScript Frameworks - AngularJS/Week 2 - Task Runners, Angular Scope, Forms and Form Validation/conFusion/Gruntfile.js
+++ 03-Front-End JavaScript Frameworks - AngularJS/Week 2 - Task Runners, Angular Scope, Forms and Form Validation/conFusion/Gruntfile.js
@@ -23,10 +23,52 @@ module.exports = function (grunt) {
                     'app/scripts/{,*/}*.js'
                 ]
             }
+        },
+        
+        // Copy files to the dist folder
+        copy: {
+            dist: {
+                cwd: 'app',
+                src: [ '**','!styles/**/*.css','!scripts/**/*.js' ],
+                dest: 'dist',
+                expand: true
+            },
+
+            fonts: {
+                files: [
+                    {
+                        //for bootstrap fonts
+                        expand: true,
+                        dot: true,
+                        cwd: 'bower_components/bootstrap/dist',
+                        src: ['fonts/*.*'],
+                        dest: 'dist'
+                    },
+                    {
+                        //for font-awesome
+                        expand: true,
+                        dot: true,
+                        cwd: 'bower_components/font-awesome',
+                        src: ['fonts/*.*'],
+                        dest: 'dist'
+                    }
+                ]
+            }
+        },
+
+        // Clean up the dist folder
+        clean: {
+            build: {
+                src: [ 'dist/']
+            }
         }
     });
     
-    grunt.registerTask('build', ['jshint']);
+    grunt.registerTask('build', [
+        'clean',
+        'jshint',
+        'copy'
+    ]);
     
     grunt.registerTask('default',['build']); // Registering the build task as the default
 };
