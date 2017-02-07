2c1592ebb27967a6612952a686c3197637358d45
Arkaitz Etxezarreta <arkaitzo@gmail.com>
2017-02-07 17:29:27 +0100

    Preparing the Distribution Folder and Files

--- 03-Front-End JavaScript Frameworks - AngularJS/Week 2 - Task Runners, Angular Scope, Forms and Form Validation/conFusion/Gruntfile.js
+++ 03-Front-End JavaScript Frameworks - AngularJS/Week 2 - Task Runners, Angular Scope, Forms and Form Validation/conFusion/Gruntfile.js
@@ -4,7 +4,9 @@ module.exports = function (grunt) {
     // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
     // Automatically load required Grunt tasks
-    require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+    });
     
     // Define the configuration for all the tasks
     grunt.initConfig({
@@ -22,6 +24,63 @@ module.exports = function (grunt) {
                     'Gruntfile.js',
                     'app/scripts/{,*/}*.js'
                 ]
+            }
+        },
+        
+        useminPrepare: {
+            html: 'app/menu.html',
+            options: {
+                dest: 'dist'
+            }
+        },
+        
+        // Concat
+        concat: {
+            options: {
+                separator: ';'
+            },
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+        
+        // Uglify
+        uglify: {
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+        
+        cssmin: {
+            dist: {}
+        },
+        
+        // Filerev
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 20
+            },
+            release: {
+                // filerev:release hashes(md5) all assets (images, js and css )
+                // in dist directory
+                files: [{
+                    src: [
+                        'dist/scripts/*.js',
+                        'dist/styles/*.css',
+                    ]
+                }]
+            }
+        },
+        
+        // Usemin
+        // Replaces all assets with their revved version in html and css files.
+        // options.assetDirs contains the directories for finding the assets
+        // according to their relative paths
+        usemin: {
+            html: ['dist/*.html'],
+            css: ['dist/styles/*.css'],
+            options: {
+                assetsDirs: ['dist', 'dist/styles']
             }
         },
         
@@ -67,7 +126,13 @@ module.exports = function (grunt) {
     grunt.registerTask('build', [
         'clean',
         'jshint',
-        'copy'
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'copy',
+        'filerev',
+        'usemin'
     ]);
     
     grunt.registerTask('default',['build']); // Registering the build task as the default
