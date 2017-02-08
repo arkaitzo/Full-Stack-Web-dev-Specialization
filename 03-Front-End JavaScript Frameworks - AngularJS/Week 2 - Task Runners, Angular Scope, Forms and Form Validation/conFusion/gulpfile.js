1181c486308e3aa6cb0087dd2cb51e59d5acf96c
Arkaitz Etxezarreta <arkaitzo@gmail.com>
2017-02-08 11:51:29 +0100

    Modifying gulpfile.js

--- 03-Front-End JavaScript Frameworks - AngularJS/Week 2 - Task Runners, Angular Scope, Forms and Form Validation/conFusion/gulpfile.js
+++ 03-Front-End JavaScript Frameworks - AngularJS/Week 2 - Task Runners, Angular Scope, Forms and Form Validation/conFusion/gulpfile.js
@@ -12,6 +12,7 @@ var gulp = require('gulp'),
     changed = require('gulp-changed'),
     rev = require('gulp-rev'),
     browserSync = require('browser-sync'),
+    ngannotate = require('gulp-ng-annotate'), // Include once you use Angular Scope
     del = require('del');
 
 // JSHint
@@ -26,7 +27,7 @@ gulp.task('usemin',['jshint'], function () {
   return gulp.src('./app/menu.html')
       .pipe(usemin({
         css:[minifycss(),rev()],
-        js: [uglify(),rev()]
+        js: [ngannotate(), uglify(),rev()] // Include ngannotate() once you use Angular Scope
       }))
       .pipe(gulp.dest('dist/'));
 });
