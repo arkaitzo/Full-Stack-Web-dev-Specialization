3db74ef17c6d153ac3aedb623e8886c11d96d52d
Arkaitz Etxezarreta <arkaitzo@gmail.com>
2017-02-09 10:57:59 +0100

    Assignment 2 - Starting point

--- 03-Front-End JavaScript Frameworks - AngularJS/Week 2 - Task Runners, Angular Scope, Forms and Form Validation/conFusion/app/scripts/app.js
+++ 03-Front-End JavaScript Frameworks - AngularJS/Week 2 - Task Runners, Angular Scope, Forms and Form Validation/conFusion/app/scripts/app.js
@@ -1,4 +1,5 @@
 "use strict";
+
 angular.module("confusionApp",[]).controller("MenuController", ["$scope", function($scope) {
     $scope.tab = 1;
     $scope.filtText = "";
@@ -41,7 +42,7 @@ angular.module("confusionApp",[]).controller("MenuController", ["$scope", functi
             description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
             comment: ''
         }
-        ];
+    ];
     //this.dishes = dishes; // Making dishes object available to the HTML code
     // (No longer necessary once you have defined $scope.dishes)
 
@@ -79,7 +80,7 @@ angular.module("confusionApp",[]).controller("MenuController", ["$scope", functi
         var channels = [{value:"tel", label:"Tel."}, {value:"email",label:"Email"}]; // Value: Email or email?
         $scope.channels = channels;
         $scope.invalidChannelSelection = false;
-}])
+    }])
 
     .controller("FeedbackController", ["$scope", function($scope) {
         $scope.sendFeedback = function() {
@@ -97,5 +98,66 @@ angular.module("confusionApp",[]).controller("MenuController", ["$scope", functi
                 console.log($scope.feedback);
             }
         };
-}])
+    }])
+
+    .controller('DishDetailController', ['$scope', function($scope) {
+        var dish={
+            name:'Uthapizza',
+            image: 'images/uthapizza.png',
+            category: 'mains', 
+            label:'Hot',
+            price:'4.99',
+            description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+            comments: [
+                {
+                    rating:5,
+                    comment:"Imagine all the eatables, living in conFusion!",
+                    author:"John Lemon",
+                    date:"2012-10-16T17:57:28.556094Z"
+                },
+                {
+                    rating:4,
+                    comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author:"Paul McVites",
+                    date:"2014-09-05T17:57:28.556094Z"
+                },
+                {
+                    rating:3,
+                    comment:"Eat it, just eat it!",
+                    author:"Michael Jaikishan",
+                    date:"2015-02-13T17:57:28.556094Z"
+                },
+                {
+                    rating:4,
+                    comment:"Ultimate, Reaching for the stars!",
+                    author:"Ringo Starry",
+                    date:"2013-12-02T17:57:28.556094Z"
+                },
+                {
+                    rating:2,
+                    comment:"It's your birthday, we're gonna party!",
+                    author:"25 Cent",
+                    date:"2011-12-02T17:57:28.556094Z"
+                }
+            ]
+        };
+        $scope.dish = dish;
+    }])
+
+    .controller('DishCommentController', ['$scope', function($scope) {
+        //Step 1: Create a JavaScript object to hold the comment from the form
+        $scope.submitComment = function () {
+            //Step 2: This is how you record the date
+            //"The date property of your JavaScript object holding the comment" = new Date().toISOString();
+            
+            // Step 3: Push your comment into the dish's comment array
+            $scope.dish.comments.push("Your JavaScript Object holding the comment");
+            
+            //Step 4: reset your form to pristine
+            //***
+            
+            //Step 5: reset your JavaScript object that holds your comment
+            //***
+        }
+    }])
 ;
