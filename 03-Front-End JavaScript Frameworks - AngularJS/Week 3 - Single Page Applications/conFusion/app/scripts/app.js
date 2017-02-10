dbb1d3cdeb98770d282d3f7642643b0db2f988ec
Arkaitz Etxezarreta <arkaitzo@gmail.com>
2017-02-10 17:44:23 +0100

    Assignment 3 - Starting point

--- 03-Front-End JavaScript Frameworks - AngularJS/Week 3 - Single Page Applications/conFusion/app/scripts/app.js
+++ 03-Front-End JavaScript Frameworks - AngularJS/Week 3 - Single Page Applications/conFusion/app/scripts/app.js
@@ -1,100 +1,71 @@
-"use strict";
+'use strict';
 
-// For using with ngRoute
-/*
-angular.module("confusionApp",["ngRoute"])
-    .config(function($routeProvider) {
-        $routeProvider
-        
-        // route for the contactus.html page
-        .when('/contactus', {
-            templateUrl : 'contactus.html',
-            controller  : 'ContactController'
-        })
-        
-        // route for the menu.html page
-        .when('/menu', {
-            templateUrl : 'menu.html',
-            controller  : 'MenuController'
-        })
-        
-        // route for the dishdetail.html page (all of them)
-        .when('/menu/:id', {
-            templateUrl : 'dishdetail.html',
-            controller  : 'DishDetailController'
-        })
-        
-        .otherwise('/contactus');
-    })
-;
-*/
-
-// For using with UI-Router
-angular.module("confusionApp",["ui.router"])
-    .config(function($stateProvider, $urlRouterProvider) {
+angular.module('confusionApp', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
         
-        // route for the home page
-        .state('app', {
-            url:'/',
-            views: {
-                'header': {
-                    templateUrl : 'views/header.html'
-                },
-                'content': {
-                    template : '<h1>To be Completed</h1>',
-                    controller  : 'IndexController'
-                },
-                'footer': {
-                    templateUrl : 'views/footer.html'
+            // route for the home page
+            .state('app', {
+                url:'/',
+                views: {
+                    'header': {
+                        templateUrl : 'views/header.html',
+                    },
+                    'content': {
+                        templateUrl : 'views/home.html',
+                        controller  : 'IndexController'
+                    },
+                    'footer': {
+                        templateUrl : 'views/footer.html',
+                    }
                 }
-            }
-        })
-        
-        // route for the aboutus page
-        .state('app.aboutus', {
-            url:'aboutus',
-            views: {
-                'content@': {
-                    template: '<h1>To be Completed</h1>',
-                    controller  : 'AboutController'
-               }
-            }
-        })
-        
-        // route for the contactus page
-        .state('app.contactus', {
-            url:'contactus',
-            views: {
-                'content@': {
-                    templateUrl : 'views/contactus.html',
-                    controller  : 'ContactController'
-                 }
-            }
-        })
 
-        // route for the menu page
-        .state('app.menu', {
-            url: 'menu',
-            views: {
-                'content@': {
-                    templateUrl : 'views/menu.html',
-                    controller  : 'MenuController'
+            })
+        
+            // route for the aboutus page
+            .state('app.aboutus', {
+                url:'aboutus',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/aboutus.html',
+                        controller  : 'AboutController'                  
+                    }
                 }
-            }
-        })
+            })
+        
+            // route for the contactus page
+            .state('app.contactus', {
+                url:'contactus',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/contactus.html',
+                        controller  : 'ContactController'                  
+                    }
+                }
+            })
 
-        // route for the dishdetail page
-        .state('app.dishdetails', {
-            url: 'menu/:id',
-            views: {
-                'content@': {
-                    templateUrl : 'views/dishdetail.html',
-                    controller  : 'DishDetailController'
-               }
-            }
-        });
+            // route for the menu page
+            .state('app.menu', {
+                url: 'menu',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/menu.html',
+                        controller  : 'MenuController'
+                    }
+                }
+            })
+
+            // route for the dishdetail page
+            .state('app.dishdetails', {
+                url: 'menu/:id',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/dishdetail.html',
+                        controller  : 'DishDetailController'
+                   }
+                }
+            });
     
-    $urlRouterProvider.otherwise('/');
-})
+        $urlRouterProvider.otherwise('/');
+    })
 ;
--- 03-Front-End JavaScript Frameworks - AngularJS/Week 3 - Single Page Applications/conFusion/app/scripts/controllers.js
+++ 03-Front-End JavaScript Frameworks - AngularJS/Week 3 - Single Page Applications/conFusion/app/scripts/controllers.js
@@ -1,92 +1,100 @@
-"use strict";
+'use strict';
 
-angular.module("confusionApp")
-    .controller("MenuController", ["$scope", "menuFactory", function($scope, menuFactory) {
-        $scope.tab = 1;
-        $scope.filtText = "";
-        $scope.showDetails = false; // Variable to be used in toggleDetails() (implemented below)
+angular.module('confusionApp')
 
-        // Call the getDishes() method of the menuFactory to get the dishes object and put it onto my $scope
-        $scope.dishes= menuFactory.getDishes(); // First inject the service into MenuController (Dependency Injection)
+        .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+            
+            $scope.tab = 1;
+            $scope.filtText = '';
+            $scope.showDetails = false;
 
-        // Implementing the select() function
-        $scope.select = function(setTab) {
-            $scope.tab = setTab;
+            $scope.dishes= menuFactory.getDishes();
 
-            // filtText
-            if (setTab === 2)
-                {$scope.filtText = "appetizer";}
-            else if (setTab === 3)
-                {$scope.filtText = "mains";}
-            else if (setTab === 4)
-                {$scope.filtText = "dessert";}
-            else
-                {$scope.filtText = "";}
-        };
+                        
+            $scope.select = function(setTab) {
+                $scope.tab = setTab;
+                
+                if (setTab === 2) {
+                    $scope.filtText = "appetizer";
+                }
+                else if (setTab === 3) {
+                    $scope.filtText = "mains";
+                }
+                else if (setTab === 4) {
+                    $scope.filtText = "dessert";
+                }
+                else {
+                    $scope.filtText = "";
+                }
+            };
 
-        // Implementing the isSelected() function
-        $scope.isSelected = function(checkTab) {
-            return ($scope.tab === checkTab);
-        };
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);
+            };
+    
+            $scope.toggleDetails = function() {
+                $scope.showDetails = !$scope.showDetails;
+            };
+        }])
 
-        // Implementing the toggleDetails() function
-        $scope.toggleDetails = function() {
-            $scope.showDetails = !$scope.showDetails;
-        };
-    }])
+        .controller('ContactController', ['$scope', function($scope) {
 
-    .controller("ContactController", ["$scope", function($scope) {
-        // This object will be also accesible by FeedbackController (because ContactController is its parent)
-        $scope.feedback = {mychannel:"", firstName:"",
-                          lastName:"", agree:false, email:""};
-        
-        var channels = [{value:"tel", label:"Tel."}, {value:"email",label:"Email"}]; // Value: Email or email?
-        $scope.channels = channels;
-        $scope.invalidChannelSelection = false;
-    }])
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+            
+            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+            
+            $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+                        
+        }])
 
-    .controller("FeedbackController", ["$scope", function($scope) {
-        $scope.sendFeedback = function() {
-            console.log($scope.feedback);
-            if ($scope.feedback.agree && ($scope.feedback.mychannel == "") && !$scope.feedback.mychannel) {
-                $scope.invalidChannelSelection = true;
-                console.log('incorrect');
-            }
-            else {
-                $scope.invalidChannelSelection = false;
-                $scope.feedback = {mychannel:"", firstName:"", lastName:"",
-                                   agree:false, email:"" };
-                $scope.feedback.mychannel="";
-                $scope.feedbackForm.$setPristine();
+        .controller('FeedbackController', ['$scope', function($scope) {
+            
+            $scope.sendFeedback = function() {
+                
                 console.log($scope.feedback);
+                
+                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+        }])
+
+        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
+
+            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
+            
+            $scope.dish = dish;
+            
+        }])
+
+        .controller('DishCommentController', ['$scope', function($scope) {
+            
+            $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+            
+            $scope.submitComment = function () {
+                
+                $scope.mycomment.date = new Date().toISOString();
+                console.log($scope.mycomment);
+                
+                $scope.mycomment.rating = Number($scope.mycomment.rating);
+                $scope.dish.comments.push($scope.mycomment);
+                
+                $scope.commentForm.$setPristine();
+                
+                $scope.mycomment = {rating:5, comment:"", author:"", date:""};
             }
-        };
-    }])
+        }])
 
-    .controller('DishDetailController', ["$scope", "$stateParams", "menuFactory",
-                                         function($scope, $stateParams, menuFactory) {
-        // Call the getDish() method of the menuFactory to get the dish object needed and put it onto my $scope
-        $scope.dish = menuFactory.getDish(parseInt($stateParams.id,10)); // Base 10 number system
-    }])
+        // implement the IndexController and About Controller here
 
-    .controller('DishCommentController', ["$scope", function($scope) {
-        //Step 1: Create a JavaScript object to hold the comment from the form. Rating by default = 5
-        $scope.comment = {author:"", rating:5, comment:""};
-        
-        $scope.submitComment = function () {
-            //Step 2: This is how you record the date
-            //"The date property of your JavaScript object holding the comment" = new Date().toISOString();
-            $scope.comment.date = new Date().toISOString();
-            
-            // Step 3: Push your comment into the dish's comment array
-            $scope.comment.rating = Number($scope.comment.rating); // Converting the rating into a number
-            $scope.dish.comments.push($scope.comment); // $scope...(JS Object holding the comment);
-            
-            //Step 4: reset your form to pristine
-            $scope.commentForm.$setPristine();
-            
-            //Step 5: reset your JavaScript object that holds your comment
-            $scope.comment = {author:"", rating:5, comment:""};
-        }
-    }])
+
 ;
