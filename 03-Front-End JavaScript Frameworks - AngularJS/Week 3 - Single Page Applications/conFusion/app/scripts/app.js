3714257d5c274ae5b647ac330d5c18cde2602ce9
Arkaitz Etxezarreta <arkaitzo@gmail.com>
2017-02-10 12:45:05 +0100

    Configuring ngRoute in 'app.js'

--- 03-Front-End JavaScript Frameworks - AngularJS/Week 3 - Single Page Applications/conFusion/app/scripts/app.js
+++ 03-Front-End JavaScript Frameworks - AngularJS/Week 3 - Single Page Applications/conFusion/app/scripts/app.js
@@ -1,3 +1,27 @@
 "use strict";
 
-angular.module("confusionApp",[]);
+angular.module("confusionApp",["ngRoute"])
+    .config(function($routeProvider) {
+        $routeProvider
+        
+        // route for the contactus.html page
+        .when('/contactus', {
+            templateUrl : 'contactus.html',
+            controller  : 'ContactController'
+        })
+        
+        // route for the menu.html page
+        .when('/menu', {
+            templateUrl : 'menu.html',
+            controller  : 'MenuController'
+        })
+        
+        // route for the dishdetail.html page (all of them)
+        .when('/menu/:id', {
+            templateUrl : 'dishdetail.html',
+            controller  : 'DishDetailController'
+        })
+        
+        .otherwise('/contactus');
+    })
+;
