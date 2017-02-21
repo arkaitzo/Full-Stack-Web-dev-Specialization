30e104d3862e71d5113f056896dd49d901e82fd0
Arkaitz Etxezarreta <arkaitzo@gmail.com>
2017-02-21 14:49:58 +0100

    Developing the conFusion App

--- 04-Multiplatform Mobile App Development with Web Technologies/Week 1 - An Intro to Hybrid Mobile App Dev Frameworks/Ionic/conFusion/www/js/app.js
+++ 04-Multiplatform Mobile App Development with Web Technologies/Week 1 - An Intro to Hybrid Mobile App Dev Frameworks/Ionic/conFusion/www/js/app.js
@@ -1,10 +1,10 @@
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'conFusion' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+// 'conFusion.controllers' is found in controllers.js
+angular.module('conFusion', ['ionic', 'conFusion.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
