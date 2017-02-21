0e16d1c5543a85f6373ac0fd750c710fde16f6b9
Arkaitz Etxezarreta <arkaitzo@gmail.com>
2017-02-21 16:16:02 +0100

    Updating the conFusion App

---
+++ 04-Multiplatform Mobile App Development with Web Technologies/Week 1 - An Intro to Hybrid Mobile App Dev Frameworks/Ionic/conFusion/www/js/services.js
@@ -0,0 +1,40 @@
+'use strict';
+
+angular.module('confusionApp')
+        .constant("baseURL", "http://localhost:3000/") // If you change your server, come here and update this baseURL
+        
+        .service('menuFactory', ["$resource", "baseURL", function($resource, baseURL) {
+            this.getDishes = function() {
+                // Fetch the data from the server - $resource(url, [paramDefaults], [actions], options);
+                return $resource(baseURL + "dishes/:id",
+                                null,
+                                {"update":{method:"PUT"}}); // We supply the parameter "update" to perform a PUT call (that in REST it means to update an existing object)
+            };
+
+            // implement a function named getPromotion
+            // that returns a selected promotion.   
+            this.getPromotion = function() {
+                return $resource(baseURL + "promotions/:id");
+                // This time we do not need to supply the parameter "update"
+            };
+        }])
+
+        .factory('corporateFactory', ["$resource", "baseURL", function($resource, baseURL) {
+            /*** REMEMBER THIS IS A FACTORY NOT A SERVICE ***/
+            var corpfac = {}; // Empty object (needed when defining a 'factory')
+            corpfac.getLeaders = function() {
+                return $resource(baseURL + "leadership/:id");
+                // This time we do not need to supply the parameter "update"
+            };
+    
+            // Returning the object (that contains both functions)
+            return corpfac;
+        }])
+
+        .service('feedbackFactory', ["$resource", "baseURL", function($resource, baseURL) {
+            this.getFeedback = function() {
+                return $resource(baseURL + "feedback/");
+                // No need to supply the parameter "save" to perform a POST call (it's already implemented)
+            };
+        }])
+;
