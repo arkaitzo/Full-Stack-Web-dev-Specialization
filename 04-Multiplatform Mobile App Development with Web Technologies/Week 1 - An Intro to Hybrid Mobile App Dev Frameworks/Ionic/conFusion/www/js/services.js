06b26c6d8d8b6b5c584911cefcb94b1833a5e038
Arkaitz Etxezarreta <arkaitzo@gmail.com>
2017-02-21 16:19:30 +0100

    Updating 'services.js'

--- 04-Multiplatform Mobile App Development with Web Technologies/Week 1 - An Intro to Hybrid Mobile App Dev Frameworks/Ionic/conFusion/www/js/services.js
+++ 04-Multiplatform Mobile App Development with Web Technologies/Week 1 - An Intro to Hybrid Mobile App Dev Frameworks/Ionic/conFusion/www/js/services.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('confusionApp')
+angular.module('conFusion.services', ['ngResource'])
         .constant("baseURL", "http://localhost:3000/") // If you change your server, come here and update this baseURL
         
         .service('menuFactory', ["$resource", "baseURL", function($resource, baseURL) {
