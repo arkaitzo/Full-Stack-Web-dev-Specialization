125d9c1d35f7bf0fc287b253249f8382030133a4
Arkaitz Etxezarreta <arkaitzo@gmail.com>
2017-02-28 15:02:41 +0100

    Updating 'app.js' to use resolve

--- 04-Multiplatform Mobile App Development with Web Technologies/Week 3 - Deploying your App/Ionic/conFusion/www/js/app.js
+++ 04-Multiplatform Mobile App Development with Web Technologies/Week 3 - Deploying your App/Ionic/conFusion/www/js/app.js
@@ -62,7 +62,15 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.servic
       views: {
         'mainContent': {
           templateUrl: 'templates/favorites.html',
-          controller: 'FavoritesController'
+          controller: 'FavoritesController',
+          resolve: {
+              dishes:  ['menuFactory', function(menuFactory){
+                  return menuFactory.query();
+              }],
+              favorites: ['favoriteFactory', function(favoriteFactory) {
+                  return favoriteFactory.getFavorites();
+              }]
+          }
         }
       }
   })
@@ -80,7 +88,12 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.servic
     views: {
       'mainContent': {
         templateUrl: 'templates/dishdetail.html',
-        controller: 'DishDetailController'
+        controller: 'DishDetailController',
+        resolve: {
+            dish: ['$stateParams','menuFactory', function($stateParams, menuFactory){
+                return menuFactory.get({id:parseInt($stateParams.id, 10)});
+            }]
+        }
       }
     }
   });
