f3ba924b13b967220ad4559ad798e43e78498da6
Arkaitz Etxezarreta <arkaitzo@gmail.com>
2017-03-03 13:35:15 +0100

    Updating 'app.js' and 'index.html' to Dismiss the Splash Screen

--- 04-Multiplatform Mobile App Development with Web Technologies/Week 4 - Accessing Native Capabilities of Devices/Ionic/conFusion/www/js/app.js
+++ 04-Multiplatform Mobile App Development with Web Technologies/Week 4 - Accessing Native Capabilities of Devices/Ionic/conFusion/www/js/app.js
@@ -4,9 +4,9 @@
 // 'conFusion' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'conFusion.controllers' is found in controllers.js
-angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.services'])
+angular.module('conFusion', ['ionic', 'ngCordova', 'conFusion.controllers', 'conFusion.services'])
 
-.run(function($ionicPlatform, $rootScope, $ionicLoading) {
+.run(function($ionicPlatform, $rootScope, $ionicLoading, $cordovaSplashscreen, $timeout) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -19,6 +19,11 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.servic
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    
+    // Hide the splash screen after 2 seconds
+    $timeout(function(){
+        $cordovaSplashscreen.hide();
+    }, 2000);
   });
 
     // Using the ui-router events to show and hide the loading message
