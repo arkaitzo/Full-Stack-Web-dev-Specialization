f5163dd1f541d75e18844b21e0fe120acf70dc19
Arkaitz Etxezarreta <arkaitzo@gmail.com>
2017-03-06 12:05:06 +0100

    Assignment 4.3 - Making the phone vibrate

--- 04-Multiplatform Mobile App Development with Web Technologies/Week 4 - Accessing Native Capabilities of Devices/Ionic/conFusion/www/js/controllers.js
+++ 04-Multiplatform Mobile App Development with Web Technologies/Week 4 - Accessing Native Capabilities of Devices/Ionic/conFusion/www/js/controllers.js
@@ -415,7 +415,7 @@ angular.module('conFusion.controllers', [])
 }])
 
 // Implementing the "FavoritesController"...
-.controller('FavoritesController', ['$scope', 'dishes', 'favorites', 'favoriteFactory', 'baseURL', '$ionicListDelegate', '$ionicPopup', '$timeout', function ($scope, dishes, favorites, favoriteFactory, baseURL, $ionicListDelegate, $ionicPopup, $timeout) {
+.controller('FavoritesController', ['$scope', 'dishes', 'favorites', 'favoriteFactory', 'baseURL', '$ionicListDelegate', '$ionicPopup', '$timeout', '$cordovaVibration', function ($scope, dishes, favorites, favoriteFactory, baseURL, $ionicListDelegate, $ionicPopup, $timeout, $cordovaVibration) {
     $scope.baseURL = baseURL;
     $scope.shouldShowDelete = false;
 
@@ -439,6 +439,9 @@ angular.module('conFusion.controllers', [])
             if (res) {
                 console.log('Ok to delete');
                 favoriteFactory.deleteFromFavorites(index);
+                // Assignment 4 - Task 3 - Making the phone vibrate
+                $cordovaVibration.vibrate(100);
+                // End of Assignment 4 - Task 3
             } else {
                 console.log('Canceled delete');
             }
