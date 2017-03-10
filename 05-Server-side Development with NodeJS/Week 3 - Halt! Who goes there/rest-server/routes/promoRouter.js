1518aa024c8359796e8850ab2e129d5329cb56cb
Arkaitz Etxezarreta <arkaitzo@gmail.com>
2017-03-10 14:22:11 +0100

    Updating 'promoRouter.js' to wire it up

--- 05-Server-side Development with NodeJS/Week 3 - Halt! Who goes there/rest-server/routes/promoRouter.js
+++ 05-Server-side Development with NodeJS/Week 3 - Halt! Who goes there/rest-server/routes/promoRouter.js
@@ -1,41 +1,65 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Dishes = require('../models/promotions');
 
 var promoRouter = express.Router();
 promoRouter.use(bodyParser.json());
 
 // URI: /
 promoRouter.route('/')
-.all(function(req,res,next) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    next();
-})
 .get(function(req,res,next) {
-    res.end('Will send all the promotion dishes to you!');
+    Promotions.find({}, function (err,promotion) {
+        if (err) throw err;
+        res.json(promotion);
+    });
 })
-.post(function(req,res,next) {
-    res.end('Will add the promotion dish: ' + req.body.name + ' with details: ' + req.body.description);    
+.post(function (req,res,next) {
+    Promotions.create(req.body, function (err,promotion) {
+        if (err) throw err;
+        console.log('\nPromotion created!');
+        var id = promotion._id;
+
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Added the promotion with id: ' + id);
+    });
 })
-.delete(function(req,res,next) {
-    res.end('Deleting all promotion dishes');
+.delete(function (req,res,next) {
+    Promotions.remove({}, function (err,resp) {
+        if (err) throw err;
+        res.json(resp);
+    });
 });
+
 
 // URI: ../:promoId
 promoRouter.route('/:promoId')
-.all(function(req,res,next) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    next();
+.get(function (req,res,next) {
+    Promotions.findById(req.params.promoId, function (err,promotion) {
+        if (err) throw err;
+        res.json(promotion);
+    });
 })
-.get(function(req,res,next) {
-    res.end('Will send details of the promotion dish: ' + req.params.promoId +' to you!');
+.put(function (req,res,next) {
+    Promotions.findByIdAndUpdate(req.params.promoId,
+                             {
+        $set: req.body
+    },
+                             {
+        new: true
+    },
+                             function (err,promotion) {
+        if (err) throw err;
+        res.json(promotion);
+    });
 })
-.put(function(req,res,next) {
-    res.write('Updating the promotion dish: ' + req.params.promoId + '\n');
-    res.end('Will update the promotion dish: ' + req.body.name +
-            ' with details: ' + req.body.description);
-})
-.delete(function(req,res,next) {
-    res.end('Deleting promotion dish: ' + req.params.promoId);
+.delete(function (req,res,next) {
+    Promotions.findByIdAndRemove(req.params.promoId, function (err,resp) {
+        if (err) throw err;
+        res.json(resp);
+    });
 });
 
 
