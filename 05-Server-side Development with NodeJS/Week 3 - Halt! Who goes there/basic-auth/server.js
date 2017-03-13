ceddea6db398d4c6b2fe36692e3c8614f14a0cfd
Arkaitz Etxezarreta <arkaitzo@gmail.com>
2017-03-13 13:24:22 +0100

    Adding Basic Authentication

--- 05-Server-side Development with NodeJS/Week 3 - Halt! Who goes there/basic-auth/server.js
+++ 05-Server-side Development with NodeJS/Week 3 - Halt! Who goes there/basic-auth/server.js
@@ -4,11 +4,50 @@ var morgan = require('morgan');
 var hostname = 'localhost';
 var port = 3000;
 
+// #1
 var app = express();
 
 app.use(morgan('dev'));
 
+// Adding basic authentication
+function auth(req,res,next) {
+    console.log(req.headers);
+    
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        err.status = 401; // Authorization has failed!
+        next(err);
+        return; // Raise the error and return
+    }
+    
+    // Otherwise, parse the authorization header
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':'); // Basic base64encodedString -> user:pass
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
+        next(); // authorized
+    } else {
+        var err = new Error('You are not authenticated!');
+        err.status = 401; // Authorization has failed!
+        next(err); // Raise the error
+    }
+}
+
+// #2
+app.use(auth); // Use the above middleware function
+
+// #3
 app.use(express.static(__dirname + '/public'));
+
+// #4 - Handle errors in this middleware
+app.use(function(err,req,res,next) {
+    res.writeHead(err.status || 500, { // Default 500 (Internal Server Error) if you do not know what else to say
+        'WWW-Authenticate': 'Basic',
+        'Content-Type': 'text/plain'
+    });
+    res.end(err.message);
+});
 
 app.listen(port, hostname, function(){
     console.log(`Server running at http://${hostname}:${port}/`);
