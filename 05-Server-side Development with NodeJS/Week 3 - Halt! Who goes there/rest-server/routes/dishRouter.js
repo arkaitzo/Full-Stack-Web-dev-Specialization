c33980d99424245ce5ea515d404d6701495b0c5d
Arkaitz Etxezarreta <arkaitzo@gmail.com>
2017-03-13 12:36:08 +0100

    Handling Comments

--- 05-Server-side Development with NodeJS/Week 3 - Halt! Who goes there/rest-server/routes/dishRouter.js
+++ 05-Server-side Development with NodeJS/Week 3 - Halt! Who goes there/rest-server/routes/dishRouter.js
@@ -65,6 +65,73 @@ dishRouter.route('/:dishId')
     });
 });
 
+// URI: ../:dishId/comments
+dishRouter.route('/:dishId/comments')
+.get(function (req,res,next) {
+    Dishes.findById(req.params.dishId, function (err,dish) {
+        if (err) throw err;
+        res.json(dish.comments);
+    });
+})
+.post(function (req,res,next) {
+    Dishes.findById(req.params.dishId, function (err,dish) {
+        if (err) throw err;
+        dish.comments.push(req.body);
+        dish.save(function (err, dish) {
+            if (err) throw err;
+            console.log('Updated Comments!');
+            res.json(dish);
+        });
+    });
+})
+.delete(function (req,res,next) {
+    Dishes.findById(req.params.dishId, function (err,dish) {
+        if (err) throw err;
+        for (var i = (dish.comments.length - 1); i >= 0; i--) {
+            dish.comments.id(dish.comments[i]._id).remove();
+        }
+        dish.save(function (err,result) {
+            if (err) throw err;
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            res.end('Deleted all comments!');
+        });
+    });
+});
+
+// URI: ../:dishId/comments/:commentId
+dishRouter.route('/:dishId/comments/:commentId')
+.get(function (req,res,next) {
+    Dishes.findById(req.params.dishId, function (err,dish) {
+        if (err) throw err;
+        res.json(dish.comments.id(req.params.commentId));
+    });
+})
+.put(function (req,res,next) {
+    // We delete the existing commment and insert the updated
+    // comment as a new comment
+    Dishes.findById(req.params.dishId, function (err,dish) {
+        if (err) throw err;
+        dish.comments.id(req.params.commentId).remove();
+        dish.comments.push(req.body);
+        dish.save(function (err, dish) {
+            if (err) throw err;
+            console.log('Updated Comments!');
+            res.json(dish);
+        });
+    });
+})
+.delete(function (req,res,next) {
+    Dishes.findById(req.params.dishId, function (err,dish) {
+        dish.comments.id(req.params.commentId).remove();
+        dish.save(function (err, resp) {
+            if (err) throw err;
+            res.json(resp);
+        });
+    });
+});
+
 
 // Export the module "dishRouter"
 module.exports = dishRouter;
