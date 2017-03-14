bba8865215f1e764e2572b7ae52575bbd7efe0ed
Arkaitz Etxezarreta <arkaitzo@gmail.com>
2017-03-14 15:36:05 +0100

    Update User Schema

--- 05-Server-side Development with NodeJS/Week 4 - Backend as a Service (BaaS)/rest-server-passport/models/user.js
+++ 05-Server-side Development with NodeJS/Week 4 - Backend as a Service (BaaS)/rest-server-passport/models/user.js
@@ -6,12 +6,25 @@ var User = new Schema(
     {
         username: String, // Even if we don't declare 'username', passportLocalMongoose will automatically insert it
         password: String, // Even if we don't declare 'password', passportLocalMongoose will automatically insert it
+        firstname: {
+            type: String,
+            default: ''
+        },
+        lastname: {
+            type: String,
+            default: ''
+        },
         admin: {
             type: Boolean,
             default: false
         }
     }
 );
+
+// Instance method for this mongoose schema - Return the full name
+User.methods.getName = function() {
+    return (this.firstname + ' ' + this.lastname);
+};
 
 User.plugin(passportLocalMongoose);
 
